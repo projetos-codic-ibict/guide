94634f50206cae77d8eceb775c8573754322fc5d
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2023-01-13 14:54:10 -0300

    Update header.php

--- app/Views/Headers/header.php
+++ app/Views/Headers/header.php
@@ -12,15 +12,15 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/guide.css">
+    <link rel="stylesheet" href="<?= PATH; ?>css/guide.css">
 
     <script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
 
-    <script src="/js/jquery-3.6.1.min.js"></script>
+    <script src="<?= PATH; ?>/js/jquery-3.6.1.min.js"></script>
 
-    <script src="/js/guide.js"></script>
+    <script src="<?= PATH; ?>/js/guide.js"></script>
     <script>
         var $path = "<?= PATH; ?>";
     </script>
